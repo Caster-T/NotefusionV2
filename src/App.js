ac7a7c3452b61a87a80aa2850eaa68683ed9bb13
ian holmm <ianholmm.28@gmail.com>
2024-09-26 18:59:52 -0300

    mas endpoints

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from './ComponentsApp/Home';
 import Canvas from './ComponentsApp/canvas';
+import Registrar from './ComponentsApp/Registrar';
+import Login from './ComponentsApp/login';
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
       <nav>
         <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-        <Link to="/Canvas">Canvas</Link>
+        <Link to="/Canvas" style={{ marginRight: '10px' }}>Canvas</Link>
+        <Link to="/Login" style={{ marginRight: '10px' }}>login</Link>
+        <Link to="/Registrar">Registrar</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Canvas" element={<Canvas />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Registrar" element={<Registrar />} />
       </Routes>
     </div>
   );
