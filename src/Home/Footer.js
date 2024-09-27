48f103127c888070b28dcc46b9c99026a5a4466f
ezechavezz <ezequielmartinchavez@gmail.com>
2024-09-27 13:02:21 -0300

    Hazlo a tu estilo V1

--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -2,12 +2,17 @@ import React from "react";
 import "../Home/HomeCss/Home.css";
 
 const Footer = () => {
-  return <div className="ContenedorFlex">
-     <p className="HijoFlex" id="FooterInicio">Inicio</p> {/*con js borrar en inicio */}
-    <p className="HijoFlex">Política de privacidad</p>
-    <p className="HijoFlex">Terminos</p>
-    <div className="HijoFlex">Copyright © 2024</div>
-  </div>
+  return (
+    <div className="ContenedorFlex">
+      <p className="HijoFlex" id="FooterInicio">
+        Inicio
+      </p>{" "}
+      {/*con js borrar en inicio */}
+      <p className="HijoFlex">Política de privacidad</p>
+      <p className="HijoFlex">Terminos</p>
+      <div className="HijoFlex">Copyright © 2024</div>
+    </div>
+  );
 };
 
 export default Footer;
