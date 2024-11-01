7d4ab13eda5294056c9ba75f66169cb6fa38d559
Luciano16Ian <ianbinder99@gmail.com>
2024-10-31 23:45:41 -0300

    Mejora parcial del login

--- src/pages/SuccesPage/SuccessPage.js
+++ src/pages/SuccesPage/SuccessPage.js
@@ -1,24 +1,53 @@
-// SuccessPage.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LogoutButton from '../Logout/Logout'; // Importa LogoutButton
+import supabase from '../Login/SupaBaseClient'; 
 import "./SuccessPage.css";
 
 function SuccessPage() {
+  const [user, setUser ] = useState(null);
   const navigate = useNavigate();
 
-  const handleGoHome = () => {
+  useEffect(() => {
+  const checkSessionAndGetUser  = async () => {
+    const { data: { session } } = await supabase.auth.getSession(); // Verifica la sesión
+    if (session) {
+      const { data } = await supabase.auth.getUser (); // Obtiene el usuario
+      setUser (data.user);
+    } else {
+      navigate("/"); // Si no hay sesión, redirige a la página principal
+    }
+  };
+
+  checkSessionAndGetUser ();
+}, [navigate]);
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
     navigate('/');
   };
 
   return (
     <div className="auth-container">
       <div className="auth-box">
-        <h1>Has iniciado sesión con éxito</h1>
-        <button className="success-button" onClick={handleGoHome}>
-          Volver a la página principal
-        </button>
-        <LogoutButton /> {/* Botón para cerrar sesión */}
+        {user ? (
+          <>
+            <h1>¡Has iniciado sesión con éxito!</h1>
+            <p>{user.email}</p>
+            <button className="return-button" onClick={() => navigate('/')}>
+              Volver a la página principal
+            </button>
+            <button className="logout-button" onClick={handleSignOut}>
+              Cerrar sesión
+            </button>
+          </>
+        ) : (
+          <>
+            <h1>No has iniciado sesión</h1>
+            <button className="return-button" onClick={() => navigate('/')}>
+              Volver a la página principal
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
